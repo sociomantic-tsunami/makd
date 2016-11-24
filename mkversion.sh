36175cdae8027718f90d1af8692f2d96ec4dd4e0
Leandro Lucarella <leandro.lucarella@sociomantic.com>
2016-11-23 23:51:21 -0300

    mkversion: Fix default date in help

--- mkversion.sh
+++ mkversion.sh
@@ -22,7 +22,7 @@ Options:
 
 -o FILE      Where to write the output (Version.d) file (default: $rev_file)
 -a AUTHOR    Author of the build (default: detected, currently $author)
--d DATE      Build date string (default: output of '$date_cmd')
+-d DATE      Build date string (default now: $date)
 -m MODULE    Module name to use in the module declaration (default: built from -o)
 -v           Be more verbose (print a message if the file was updated)
 -p           Only print this repository version and exit
